923ea7235d3a4d5814d0cf4ba44b95484479c33e
신승민 <shinseungmin10@gmail.com>
2022-01-13 01:36:01 +0900

    2.0 firebase 연결

---
+++ 16.twitter_clone/nwitter/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDW12oii92TMoGG4PSspiMVGtxIl3EDurI",
+    authDomain: "nwitter-58565.firebaseapp.com",
+    projectId: "nwitter-58565",
+    storageBucket: "nwitter-58565.appspot.com",
+    messagingSenderId: "973529504523",
+    appId: "1:973529504523:web:22f619be0332763740200e"
+  };
+
+  // Initialize Firebase
+export default initializeApp(firebaseConfig);
