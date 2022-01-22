742272324559f91e8f06a55f61c6a1e466e80ea7
신승민 <shinseungmin10@gmail.com>
2022-01-22 23:55:20 +0900

    3.0 환경변수

--- 16.twitter_clone/nwitter/src/firebase.js
+++ 16.twitter_clone/nwitter/src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDW12oii92TMoGG4PSspiMVGtxIl3EDurI",
-    authDomain: "nwitter-58565.firebaseapp.com",
-    projectId: "nwitter-58565",
-    storageBucket: "nwitter-58565.appspot.com",
-    messagingSenderId: "973529504523",
-    appId: "1:973529504523:web:22f619be0332763740200e"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 
   // Initialize Firebase
