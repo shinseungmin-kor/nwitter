9cfaf1dc2961066dd3d8752e489cf172e1e776d7
신승민 <shinseungmin10@gmail.com>
2022-01-27 00:49:00 +0900

    7.0 로그아웃

--- 16.twitter_clone/nwitter/src/routes/Profile.js
+++ 16.twitter_clone/nwitter/src/routes/Profile.js
@@ -1,3 +1,17 @@
+import { authService } from "myBase";
 import React from "react";
+import { useHistory } from "react-router";
 
-export default () => <span>Profile</span>;
+export default () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/")
+    }
+
+    return (
+        <>
+          <button onClick={onLogOutClick}>Log out</button>
+        </>
+    )
+}
