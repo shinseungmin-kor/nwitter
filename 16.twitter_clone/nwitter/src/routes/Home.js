265f530e79a93bb51abb5882a1f6cf19e3d77118
신승민 <shinseungmin10@gmail.com>
2022-02-02 02:44:34 +0900

    이미지 업로드 중 오류발견

--- 16.twitter_clone/nwitter/src/routes/Home.js
+++ 16.twitter_clone/nwitter/src/routes/Home.js
@@ -1,11 +1,12 @@
-import { dbService } from "myBase";
+import { dbService, storageService } from "myBase";
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Nweet from "../components/Nweet";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
- 
+  const [attachment, setAttachment] = useState();
   
   useEffect(() => {
     dbService.collection("nweets").onSnapshot((snapshot) => {
@@ -17,14 +18,17 @@ const Home = ({ userObj }) => {
     });
   },[]);
 
-  const onSubmit = async(event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("nweets").add({
-        text: nweet,
-        createdAt: Date.now(),
-        creatorId: userObj.uid,
-    });
-    setNweet("");
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+    const response = await fileRef.putString(attachment, "data_url");
+    console.log(response)
+    // await dbService.collection("nweets").add({
+    //     text: nweet,
+    //     createdAt: Date.now(),
+    //     creatorId: userObj.uid,
+    // });
+    // setNweet("");
   };
   const onChange = (event) => {
     const {
@@ -33,6 +37,26 @@ const Home = ({ userObj }) => {
     setNweet(value);
   };
   // console.log(nweets) 
+
+  const onFileChange = (event) => {
+    // console.log(event.target.files)
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    }
+    reader.readAsDataURL(theFile);
+  };
+
+  const onClearAttachment = () => {
+    setAttachment(null)
+  }
 
   return (
     <div>
@@ -44,14 +68,21 @@ const Home = ({ userObj }) => {
           placeholder="What`s on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
-          <Nweet 
-          key={nweet.id} 
-          nweetObj={nweet}
-          isOwner={nweet.creatorId === userObj.uid}
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
           />
         ))}
       </div>
